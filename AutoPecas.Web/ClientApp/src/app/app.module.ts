8041443e17608bbf455fad5a2d8ce4cd79ae541e
Diego La Torre <df.lima98@gmail.com>
2020-04-28 00:03:00 -0300

    TEstando o service.

--- AutoPecas.Web/ClientApp/src/app/app.module.ts
+++ AutoPecas.Web/ClientApp/src/app/app.module.ts
@@ -6,6 +6,15 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { NgZorroAntdModule, NZ_I18N, en_US, NZ_ICONS } from 'ng-zorro-antd';
+import { IconDefinition } from '@ant-design/icons-angular';
+import * as AllIcons from '@ant-design/icons-angular/icons';
+
+const antDesignIcons = AllIcons as {
+  [key: string]: IconDefinition;
+};
+const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key])
+
 
 @NgModule({
   declarations: [
@@ -19,9 +28,10 @@ import { HomeComponent } from './home/home.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' }
-    ])
+    ]),
+    NgZorroAntdModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }, { provide: NZ_ICONS, useValue: icons }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- AutoPecas.Web/ClientApp/src/app/home/home.component.ts
+++ AutoPecas.Web/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PecaService } from '../service/peca.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  pecas$: Observable<any>;
+
+  constructor(
+    private pecaService: PecaService
+  ) { }
+
+  ngOnInit() {
+    this.pecas$ = this.pecaService.listar();
+  }
 }

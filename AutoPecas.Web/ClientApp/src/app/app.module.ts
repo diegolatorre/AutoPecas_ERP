8b4d5d5f452989f37673f18a249aac418e717465
Diego La Torre <df.lima98@gmail.com>
2020-05-03 18:57:47 -0300

    Criação de modulo do ngZorro

--- AutoPecas.Web/ClientApp/src/app/app.module.ts
+++ AutoPecas.Web/ClientApp/src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-
-import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NgZorroModule } from './shared/ngZorro.module';
 
 @NgModule({
   declarations: [
@@ -23,7 +22,7 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
     ]),
-    NzButtonModule
+    NgZorroModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ AutoPecas.Web/ClientApp/src/app/shared/ngZorro.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
+@NgModule({
+  imports: [
+    NzButtonModule
+  ],
+  exports: [
+    NzButtonModule
+  ]
+})
+export class NgZorroModule { }

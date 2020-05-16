2717fc60f832b44b67ff8e772c7b8d0732e7720b
Diego Felipe La Torre Lima <diego.f.lima@kroton.com.br>
2020-05-16 20:55:05 -0300

    criação do componente de listagem de contato

--- AutoPecas.Web/ClientApp/src/app/contato/contato.module.ts
+++ AutoPecas.Web/ClientApp/src/app/contato/contato.module.ts
@@ -2,9 +2,10 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ContatoRoutingModule, EntryComponents, RoutedComponents } from './contato-routing.module';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { TabelaContatoComponent } from './tabela-contato/tabela-contato.component';
 
 @NgModule({
-  declarations: [RoutedComponents],
+  declarations: [RoutedComponents, TabelaContatoComponent],
   entryComponents: [EntryComponents],
   imports: [
     ContatoRoutingModule,
---
+++ AutoPecas.Web/ClientApp/src/app/contato/tabela-contato/tabela-contato.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabela-contato',
+  templateUrl: './tabela-contato.component.html',
+  styleUrls: ['./tabela-contato.component.css']
+})
+export class TabelaContatoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

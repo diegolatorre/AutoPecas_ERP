ec2b652018ee99cad6e67de16498937dd2c1a74d
Tales Henrique Bueno Rodrigues <talesbueno06@gmail.com>
2020-06-02 21:42:01 -0300

    Criação modulo venda

---
+++ AutoPecas.Service/VendaService.cs
@@ -0,0 +1,28 @@
+﻿using Autopecas.Infra.Data;
+using AutoPecas.Core.Model;
+using System.Threading.Tasks;
+
+namespace AutoPecas.Service
+{
+    public class VendaService
+    {
+        private readonly AutoPecasDbContext _AutoPecasDbContext;
+
+        public VendaService(AutoPecasDbContext autoPecasDbContext) => _AutoPecasDbContext = autoPecasDbContext;
+
+        public async Task<int> Venda(Venda venda)
+        {
+            _AutoPecasDbContext.Add(venda);
+            return await _AutoPecasDbContext.SaveChangesAsync();
+        }
+
+        public async Task<int> UpdateVenda(Venda venda, int id)
+        {
+            if (venda.Id != id)
+                return 0;
+
+            _AutoPecasDbContext.Update(venda);
+            return await _AutoPecasDbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ Autopecas.Infra.Data/Map/ProdutoVendaMap.cs
@@ -0,0 +1,59 @@
+﻿using AutoPecas.Core.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Text;
+
+namespace Autopecas.Infra.Data.Map
+{
+    internal class ProdutoVendaMap : IEntityTypeConfiguration<ProdutoVenda>
+    {
+        public void Configure(EntityTypeBuilder<ProdutoVenda> builder)
+        {
+            builder
+                .ToTable("PRODUTOVENDA")
+                .HasKey(p => p.IdProdutoVenda);
+
+            builder
+                .Property(p => p.IdVenda)
+                .HasColumnName("IDVENDA")
+                .UseIdentityColumn()
+                .IsRequired();
+
+            builder
+                .Property(p => p.IdProduto)
+                .HasColumnName("IDPRODUTO")
+                .UseIdentityColumn()
+                .IsRequired();
+
+            builder
+                .Property(p => p.IdProdutoVenda)
+                .HasColumnName("IDPRODUTOVENDA")
+                .UseIdentityColumn()
+                .IsRequired();
+
+            builder
+                .Property(p => p.Quantidade)
+                .HasColumnName("QUANTIDADE")
+                .UseIdentityColumn()
+                .IsRequired();
+
+            builder
+                .Property(p => p.Desconto)
+                .HasColumnName("DESCONTO")
+                .UseIdentityColumn()
+                .IsRequired();
+
+            builder
+                .Property(p => p.ValorFinal)
+                .HasColumnName("VALORFINAL")
+                .UseIdentityColumn()
+                .IsRequired();
+
+            builder
+                .HasOne(p => p.Venda)
+                .WithMany(p => p.Produtos)
+                .HasForeignKey(p => p.IdVenda);
+        }
+    }
+}

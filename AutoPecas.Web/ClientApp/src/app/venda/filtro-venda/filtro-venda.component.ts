b63e03bba80180e0ac11e77aa7b759470e9fb3ab
Diego Felipe La Torre Lima <diego.f.lima@kroton.com.br>
2020-06-09 18:59:10 -0300

    Implementação do filtro de venda.

--- AutoPecas.Web/ClientApp/src/app/venda/filtro-venda/filtro-venda.component.ts
+++ AutoPecas.Web/ClientApp/src/app/venda/filtro-venda/filtro-venda.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { NzModalRef } from 'ng-zorro-antd/modal';
 import { Contato } from 'src/app/model/contato/contato.model';
+import { StatusVendaEnum } from 'src/app/model/enum/statusVenda.enum';
 
 @Component({
   selector: 'app-filtro-venda',
@@ -12,7 +13,8 @@ export class FiltroVendaComponent implements OnInit {
   filtroForm = new FormGroup(
   {
       idContato: new FormControl(null),
-      status: new FormControl(null),
+      statusAberto: new FormControl(true),
+      statusFinalizada: new FormControl(true),
       dataAberturaInicial: new FormControl(null),
       dataAberturaFinal: new FormControl(null),
       dataFinalizacaoInicial: new FormControl(null),
@@ -21,9 +23,19 @@ export class FiltroVendaComponent implements OnInit {
       valorFinal: new FormControl(50000),
   });
 
+  statusVendaEnum = StatusVendaEnum;
+
+  statusCheckbox = [
+    { label: 'Em Aberto', value: this.statusVendaEnum.Aberta, checked: true, disabled: false },
+    { label: 'Finalizada', value: this.statusVendaEnum.Finalizada, checked: true, disabled: false },
+  ];
+
   minRange = 0;
   maxRange = 50000;
   rangeValue = [0, 50000];
+
+  dateRangeAbertura = [];
+  dateRangeFinalizacao = [];
 
   @Input() filtro?: any;
 
@@ -34,6 +46,9 @@ export class FiltroVendaComponent implements OnInit {
 
   limpar(): void {
     this.filtroForm.reset();
+    this.rangeValue = [0, 50000];
+    this.dateRangeAbertura = [];
+    this.dateRangeFinalizacao = [];
   }
 
   filtrar(): void {
@@ -44,10 +59,49 @@ export class FiltroVendaComponent implements OnInit {
     this.filtroForm.get('idContato').setValue(contato.id);
   }
 
-  onAfterChange(valor: any) {
-    this.filtroForm.get('valorInicial').setValue(valor[0]);
-    this.filtroForm.get('valorFinal').setValue(valor[1]);
-    console.log(this.filtroForm.value);
+  selecionaValor(valor: any) {
+    let valorMin, valorMax;
+
+    if (valor[0] >= valor[1]) {
+      valorMax = valor[0];
+      valorMin = valor[1];
+    } else {
+      valorMax = valor[1];
+      valorMin = valor[0];
+    }
+
+    this.filtroForm.get('valorInicial').setValue(valorMin);
+    this.filtroForm.get('valorFinal').setValue(valorMax);
+  }
+
+  selecionaDataAbertura(data: any) {
+    this.filtroForm.get('dataAberturaInicial').setValue(data[0]);
+    this.filtroForm.get('dataAberturaFinal').setValue(data[1]);
+  }
+
+  selecionaDataFinalizacao(data: any) {
+    this.filtroForm.get('dataFinalizacaoInicial').setValue(data[0]);
+    this.filtroForm.get('dataFinalizacaoFinal').setValue(data[1]);
+  }
+
+  selecionaStatus() {
+    this.filtroForm.get('statusAberto').setValue(this.statusCheckbox[0].checked);
+    this.filtroForm.get('statusFinalizada').setValue(this.statusCheckbox[1].checked);
+    if (!this.statusCheckbox[0].checked) {
+      this.statusCheckbox[1].disabled = true;
+    }
+
+    if (this.statusCheckbox[0].checked) {
+      this.statusCheckbox[1].disabled = false;
+    }
+
+    if (!this.statusCheckbox[1].checked) {
+      this.statusCheckbox[0].disabled = true;
+    }
+
+    if (this.statusCheckbox[1].checked) {
+      this.statusCheckbox[0].disabled = false;
+    }
   }
 
 }
